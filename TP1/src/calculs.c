c6390a4b2228622953339db508c736d4bab0e34f
kangninathan <kangninathan@gmail.com>
2026-01-29 11:25:46 +0000

    TP1 : exercice 1.8 switch operations

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,69 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 12;
+    int num2 = 5;
+    char op = '+';   // change pour tester : + - * / % & | ~
+
+    int resultat;
+
+    switch (op) {
+
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zero\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zero\n");
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            // opérateur unaire : s'applique sur un seul nombre
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Operateur '%c' invalide\n", op);
+            break;
+    }
+
+    return 0;
+}
 

96b29477ba10af890a873819b3b392d5784b90c7
nathan <kangninathan@gmail.com>
2026-01-29 11:03:23 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -2,37 +2,29 @@
 
 int main() {
 
-    // ----- CHAR -----
+    
     signed char sc = -10;
     unsigned char uc = 250;
 
-    // ----- SHORT -----
     signed short ss = -300;
     unsigned short us = 60000;
 
-    // ----- INT -----
     signed int si = -20000;
     unsigned int ui = 4000000000U;
 
-    // ----- LONG INT -----
     signed long sl = -2000000000L;
     unsigned long ul = 4000000000UL;
 
-    // ----- LONG LONG INT -----
     signed long long sll = -5000000000LL;
     unsigned long long ull = 10000000000ULL;
 
-    // ----- FLOAT -----
     float f = 3.14f;
 
-    // ----- DOUBLE -----
     double d = 2.718281828;
 
-    // ----- LONG DOUBLE -----
     long double ld = 1.23456789012345L;
 
 
-    // ----- AFFICHAGE -----
     printf("signed char = %d\n", sc);
     printf("unsigned char = %u\n", uc);
 

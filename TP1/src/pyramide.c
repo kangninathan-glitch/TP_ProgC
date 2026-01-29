9faafec95f6a97342c2e35c29a3515c67efb6308
nathan <kangninathan@gmail.com>
2026-01-29 12:33:18 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -2,22 +2,22 @@
 
 int main() {
 
-    int n = 5; // hauteur de la pyramide
+    int n = 5; 
 
-    // boucle sur les lignes
+    
     for (int i = 1; i <= n; i++) {
 
-        // 1) afficher les espaces pour centrer
+        
         for (int j = 1; j <= n - i; j++) {
             printf(" ");
         }
 
-        // 2) afficher les nombres croissants (1 → i)
+        
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
 
-        // 3) afficher les nombres décroissants (i-1 → 1)
+        
         for (int j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }

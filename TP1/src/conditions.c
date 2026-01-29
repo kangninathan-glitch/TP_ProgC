102649255726bff2ca357693d2420a73aea489cb
nathan <kangninathan@gmail.com>
2026-01-29 12:14:32 +0100

    Update conditions.c

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -6,17 +6,17 @@ int main() {
 
     for (int i = 1; i <= 1000; i++) {
 
-        // Si divisible par 11 → on saute au nombre suivant
+        
         if (i % 11 == 0) {
             continue;
         }
 
-        // Si divisible par 5 ou 7 → on ajoute à la somme
+        
         if (i % 5 == 0 || i % 7 == 0) {
             somme += i;
         }
 
-        // Si la somme dépasse 5000 → on arrête
+        
         if (somme > 5000) {
             break;
         }

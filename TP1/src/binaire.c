54ea060da1cef8607b3d3d34f48bb2aba7b689b9
nathan <kangninathan@gmail.com>
2026-01-29 12:30:29 +0100

    Update binaire.c

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -3,12 +3,12 @@
 void afficher_binaire(int n) {
     printf("%d en binaire = ", n);
 
-    // parcourir les 32 bits d'un int
+    
     for (int i = 31; i >= 0; i--) {
         int bit = (n >> i) & 1;
         printf("%d", bit);
 
-        // espace pour lisibilité toutes les 4 positions
+        
         if (i % 4 == 0) {
             printf(" ");
         }

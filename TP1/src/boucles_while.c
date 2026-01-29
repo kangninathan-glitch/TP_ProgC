c56fe990d802be949a5560e8450dd1fde9a142ff
nathan <kangninathan@gmail.com>
2026-01-29 11:58:34 +0100

    Update boucles_while.c

--- TP1/src/boucles_while.c
+++ TP1/src/boucles_while.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int compteur = 5;   // change la valeur pour tester
+    int compteur = 5;   
 
     int i = 1;
     while (i <= compteur) {
@@ -10,16 +10,16 @@ int main() {
         int j = 1;
         while (j <= i) {
 
-            // Si on est sur la dernière ligne → seulement des étoiles
+            
             if (i == compteur) {
                 printf("* ");
             }
-            // Sinon : alterne *, #, *, # ...
+            
             else {
                 if (j % 2 == 1)
                     printf("* ");
                 else
-                    printf("# ");
+                    printf("+ ");
             }
 
             j++;
